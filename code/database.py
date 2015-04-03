451aa749bb6121ca4661048fc869b4d1442a8bbc
Frederico Castro <castro.112358@gmail.com>
2015-04-03 19:13:40 -0400

    Database class working

--- code/database.py
+++ code/database.py
@@ -4,7 +4,7 @@ import os
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
-class PiDatabase:
+class Database:
 
   def __init__(self, path_to_database):
     self.conn = sqlite3.connect(os.path.join(dir_path, path_to_database))
@@ -14,7 +14,8 @@ class PiDatabase:
     self.cur.execute("CREATE TABLE images(dateTime TEXT, path_to_image TEXT, latitude TEXT, longitude TEXT, altitude TEXT) ")
 
   def dataEntry(self, data):
+    print data
     with self.conn:
-      self.cur.execute("INSERT INTO images (dateTime, path_to_image, latitude, longitude, altitude) VALUES( ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5]))
+      self.cur.execute("INSERT INTO images (dateTime, path_to_image, latitude, longitude, altitude) VALUES( ?, ?, ?, ?, ?)", (data['dateTime'], data['path_to_image'], data['latitude'], data['longitude'], data['altitude']))
       self.conn.commit()
       print("Image successfully recorded")
