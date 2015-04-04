19610d13583756f5cbcf205353fdc89aea3b8f57
Frederico Castro <castro.112358@gmail.com>
2015-04-03 20:39:01 -0400

    changed script

--- www/images_db.php
+++ www/images_db.php
@@ -1,7 +1,20 @@
 <?php
-$db = new SQLite3('/database/pi_database.db');
-$results = $db->query('SELECT * FROM images');
-while ($row = $results->fetchArray()) {
-var_dump($row);
+/*
+* Code to query an SQLite database and return
+* results as JSON.
+*/
+
+// Specify your sqlite database name and path //
+$dir = 'sqlite:database/pi_database.db';
+
+// Instantiate PDO connection object and failure msg //
+$dbh = new PDO($dir) or die("cannot open database");
+
+// Define your SQL statement //
+$query = "SELECT * FROM images";
+
+// Iterate through the results and pass into JSON encoder //
+foreach ($dbh->query($query) as $row) {
+echo json_encode($row[0]);
 }
 ?>
