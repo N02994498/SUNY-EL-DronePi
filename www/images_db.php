18349ee5c1e43f51c41e30c3f5a4003addf439da
Frederico Castro <castro.112358@gmail.com>
2015-04-03 21:26:44 -0400

    fix

--- www/images_db.php
+++ www/images_db.php
@@ -1,8 +1,6 @@
 <?php
 $db = new SQLite3('database/pi_database.db');
-print_r($db);
 $results = $db->query('SELECT * FROM images');
-print_r($results);
 while ($row = $results->fetchArray()) {
 var_dump($row);
 }
