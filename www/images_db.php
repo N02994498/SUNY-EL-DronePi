1d9b1c6716d50de51e6b24a600661c967f6733f6
Frederico Castro <castro.112358@gmail.com>
2015-04-03 20:34:10 -0400

    typo fix

--- www/images_db.php
+++ www/images_db.php
@@ -1,5 +1,6 @@
-< ?php
-$db = new SQLite3('database/pi_database.db');
+<?php
+echo 'hi';
+$db = new SQLite3('./database/pi_database.db');
 $results = $db->query('SELECT * FROM images');
 while ($row = $results->fetchArray()) {
 var_dump($row);
